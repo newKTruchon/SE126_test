2d7c7a6590411b11673544b29e17bfa3f2445d17
Truchon <katie.truchon@gmail.com>
2021-02-10 12:18:36 -0500

    demo for remote repo update

---
+++ w5-listPractice/findTheX.py
@@ -0,0 +1,1 @@
+#Build a program to store the following file data into a list (one field of data = 1 list needed), then process the list and find the record that holds the "X" (capital X string value).  When finished, print the index of where the X was located. 
