745f6304d2eb966b68688caac3615e64c9d2145b
Truchon <katie.truchon@gmail.com>
2021-02-10 12:21:51 -0500

    started practice 1

--- w5-listPractice/findTheX.py
+++ w5-listPractice/findTheX.py
@@ -1,1 +1,14 @@
 #Build a program to store the following file data into a list (one field of data = 1 list needed), then process the list and find the record that holds the "X" (capital X string value).  When finished, print the index of where the X was located. 
+
+import csv 
+
+
+charList = []
+
+with open("C:/Users/KTRUCHON/Desktop/SE126_test/w5-listPractice/listPractice1.csv") as csvfile:
+
+    file = csv.reader(csvfile)
+
+    for rec in file:
+
+        print(rec[0])
